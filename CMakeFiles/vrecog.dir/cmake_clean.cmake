5792f61ebb941f1487cff2790de2ce557cbb89d2
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2020-03-18 05:10:07 -0300

    Initial commit

---
+++ CMakeFiles/vrecog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vrecog.dir/src/Main.cpp.o"
+  "vrecog.pdb"
+  "vrecog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vrecog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
