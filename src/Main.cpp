ae06e7d91a1bc36418f71c8b11ddadb13b05d07a
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2020-03-20 18:21:03 -0300

    Removed unused includes and fixed indentation

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,67 +1,61 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <cctype>
-#include <thread>
-#include <chrono>
 #include "mqtt/async_client.h"
 #include "json.hpp"
 
 using namespace std;
 using json = nlohmann::json;
 
-const string SERVER_ADDRESS	{ "tcp://broker.mqttdashboard.com:1883" };
-const string CLIENT_ID		{ "listener-cpp" };
-const string TOPIC 			{ "topic/voice_recog" };
+const string SERVER_ADDRESS   { "tcp://broker.mqttdashboard.com:1883" };
+const string CLIENT_ID        { "listener-cpp" };
+const string TOPIC            { "topic/voice_recog" };
 
 const int  QOS = 1;
 
 /////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]){
-	
-	std::ifstream i(argv[1]);
-   json j = json::parse(i);
-   cout << std::setw(4) << j << '\n';
 
-	mqtt::connect_options connOpts;
-	connOpts.set_keep_alive_interval(20);
-	connOpts.set_clean_session(true);
+   std::ifstream i(argv[1]);
+   json cmdList = json::parse(i);
+   cout << std::setw(4) << cmdList << '\n';
 
-	mqtt::async_client cli(SERVER_ADDRESS, CLIENT_ID);
+   mqtt::connect_options connOpts;
+   connOpts.set_keep_alive_interval(20);
+   connOpts.set_clean_session(true);
 
-	try {
-		cout << "Connected as '" << CLIENT_ID << "' to '" << SERVER_ADDRESS << "'" << endl;
-		cli.connect(connOpts)->wait();
-		cli.start_consuming();
-		cli.subscribe(TOPIC, QOS)->wait();
+   mqtt::async_client cli(SERVER_ADDRESS, CLIENT_ID);
 
-		// Consume messages
+   try {
+      cout << "Connected as '" << CLIENT_ID << "' to '" << SERVER_ADDRESS << "'" << endl;
+      cli.connect(connOpts)->wait();
+      cli.start_consuming();
+      cli.subscribe(TOPIC, QOS)->wait();
 
-		while (true) {
-			auto msg = cli.consume_message();
-			if (!msg) break;
-			if (!cmdList[msg->to_string()].empty())
-			   cout << cmdList[msg->to_string()] << endl;
-			else
-			   cout << "\"" << msg->to_string() << "\" (invalid command) was received from '" << msg->get_topic() << "'" << endl;
-		}
+      // Consume messages
 
-		// Disconnect
+      while (true) {
+         auto msg = cli.consume_message();
+         if (!msg) break;
+         if (!cmdList[msg->to_string()].empty())
+            cout << cmdList[msg->to_string()] << endl;
+         else
+            cout << "\"" << msg->to_string() << "\" (invalid command) was received from '" << msg->get_topic() << "'" << endl;
+      }
 
-		cout << "\nShutting down and disconnecting from the MQTT server..." << flush;
-		cli.unsubscribe(TOPIC)->wait();
-		cli.stop_consuming();
-		cli.disconnect()->wait();
-		cout << "OK" << endl;
-	}
-	catch (const mqtt::exception& exc) {
-		cerr << exc.what() << endl;
-		return 1;
-	}
+      // Disconnect
 
- 	return 0;
+      cout << "\nShutting down and disconnecting from the MQTT server..." << flush;
+      cli.unsubscribe(TOPIC)->wait();
+      cli.stop_consuming();
+      cli.disconnect()->wait();
+      cout << "OK" << endl;
+   }
+   catch (const mqtt::exception& exc) {
+      cerr << exc.what() << endl;
+      return 1;
+   }
+
+   return 0;
 }
